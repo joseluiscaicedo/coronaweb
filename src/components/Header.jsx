88175405eca1ecf22125575bde30ede6729af048
jose caicedo <joseluiscl@gmail.com>
2021-07-02 21:48:49 -0500

    boilerplate josec

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// import ReactDOM from 'react-dom';
+import '../assets/styles/App.scss';
+
+const Header = () => {
+  return (
+    <h1>Header</h1>
+  );
+};
+
+export default Header;
