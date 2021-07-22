1c58708c44df3956cc2873230e4bb6bf3b34f7f8
jose caicedo <joseluiscl@gmail.com>
2021-07-22 09:24:36 -0500

    add react router

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,29 +1,22 @@
 import React from 'react';
-// import { useAuth0 } from '@auth0/auth0-react';
-import Header from '../components/Header';
-import Search from '../components/Search';
-import Footer from '../components/Footer';
-import Categories from '../components/Categories';
-import Carousel from '../components/Carousel';
-import CarouselItem from '../components/CarouselItem';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import NotFound from '../components/NotFound';
+import Home from '../page/Home';
+import Layout from '../components/Layout';
+
 import '../assets/styles/App.scss';
 
-// const { isAuthenticated } = useAuth0();
 const App = () => {
   return (
-    <div className='App'>
-      <Header />
-      <Search />
-      <Categories>
-        <Carousel>
-          <CarouselItem />
-        </Carousel>
-      </Categories>
-      <NotFound />
-
-      <Footer />
-    </div>
+    <Router>
+      <Layout>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route component={NotFound} />
+        </Switch>
+      </Layout>
+    </Router>
   );
 };
 
