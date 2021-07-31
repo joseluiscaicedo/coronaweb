0ecd95d1685bc5f5c7b617f1496a8c0d20687cc5
jose caicedo <joseluiscl@gmail.com>
2021-07-31 09:02:14 -0500

    add search functionality

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,13 +1,50 @@
-import React from 'react';
+/* eslint-disable react/display-name */
+import React, { useState } from 'react';
+import CarouselItem from './CarouselItem';
+
 import '../assets/styles/Search.scss';
 
-const Search = () => (
-  <section className='search'>
-    <h2 className='search__title'>Search by Country</h2>
-    <label id='Search' htmlFor='Search' rol='tab'>
-      <input type='text' className='input' placeholder='Write a country...' />
-    </label>
-  </section>
-);
+const SearchTerm = ({setSearchCountries})=>(event)=> {
+  setSearchCountries(event.target.value)
+}
+const filterTerm = ({ searchCountries }) => (event) =>{
+  if(searchCountries==='')return event;
+  if (event.Country.toLowerCase().includes(searchCountries.toLowerCase())) {
+    return event;
+  }
+};
+const mapCountries =({ CarouselItem }) => ({ ...CovidCountries }) => <CarouselItem key={CovidCountries.ID} {...CovidCountries}/>
 
+
+const Search = ({CovidCountries}) => {
+  const [searchCountries, setSearchCountries] = useState('');
+
+  return(
+  <>
+    <section className='search'>
+      <h1 className='search__title'>Search by Country</h1>
+      <label
+      aria-labelledby='WriteaCountry'
+      className='search__label'
+      id='Search'
+      htmlFor='WriteaCountry'
+      rol='tab'
+      > Write a Country
+        <input
+        id='WriteaCountry'
+        name='input'
+        type='text'
+        className='label__input'
+        autoComplete = 'Write a country'
+        rol='tab'
+        onChange={SearchTerm({ setSearchCountries })}
+        />
+      </label>
+    </section>
+    <section className='ShowCountries'>
+      {CovidCountries.filter(filterTerm({ searchCountries })).map(mapCountries({ CarouselItem }))}
+    </section>
+  </>
+  );
+}
 export default Search;
