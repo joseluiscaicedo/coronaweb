8f10e9c6e0c695997b29f25e5cd8cf20e8199121
jose caicedo <joseluiscl@gmail.com>
2021-07-31 09:09:10 -0500

    add blue color to paragraphs

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -5,9 +5,9 @@ const MainSection = () => {
   return (
       <section className='banner' id='banner' role='banner'>
         <div className='banner__container'>
-          <h2>COVID-19 Help Center</h2>
+          <h1>COVID-19 Help Center</h1>
           <p className='container__firstparagraph'>As your healthcare partner, Coronaweb gives you all the information you need about COVID-19 in one place.</p>
-          <p>Please, Login o Register to keep update</p>
+          <p>Please login or register to stay updated</p>
         </div>
       </section>
   )
