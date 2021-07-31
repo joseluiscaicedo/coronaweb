59f1f9a9a014992fa72b4b9c255d83b08d70a701
jose caicedo <joseluiscl@gmail.com>
2021-07-31 09:51:05 -0500

    add responsive styles and design

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -7,16 +7,16 @@ const delorean = 'https://res.cloudinary.com/josecaicedo/image/upload/v162742697
 const NotFound = () => (
   <body>
     <section className='notFound'>
-      <div className='notFound__img'>
-        <img src={backtoHome} alt='Back to the Homepage' />
-        <img src={delorean} alt='El Delorean, El Doc y Marti McFly' />
+      <div className='notFound__imgContainer'>
+        <img className='imgContainer__img' src={backtoHome} alt='Back to the Homepage' />
+        <img className='imgContainer__img' src={delorean} alt='El Delorean, El Doc y Marti McFly' />
       </div>
       <div className='notFound__container'>
         <h1 className='container__h1'>404</h1>
         <h2 className='container__h2'>PAGE NOT FOUND</h2>
         <h3 className='container__h3'>BACK TO HOME?</h3>
-        <a href='/' className='container__anchor'>YES</a>
-        <a className='container__anchor' href='https://www.youtube.com/watch?v=G3AfIvJBcGo'>NO</a>
+        <a aria-describedby='GoToHome' href='/' className='container__anchor'>YES</a>
+        <a aria-describedby='GoOut' className='container__anchor' href='https://www.youtube.com/watch?v=G3AfIvJBcGo'>NO</a>
       </div>
     </section>
   </body>
